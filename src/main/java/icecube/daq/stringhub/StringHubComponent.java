cc8fcec9d840ecd07485e70ce82607f558f50eb2
Pat Toale <toale@phys.psu.edu>
2007-03-30 09:22:20 -0500

    Hooked up the trigger configuration in the configuring() method.

--- src/main/java/icecube/daq/stringhub/StringHubComponent.java
+++ src/main/java/icecube/daq/stringhub/StringHubComponent.java
@@ -26,7 +26,9 @@ import icecube.daq.util.DOMRegistry;
 import icecube.daq.util.DeployedDOM;
 import icecube.daq.trigger.control.StringTriggerHandler;
 import icecube.daq.trigger.control.IStringTriggerHandler;
-import icecube.daq.trigger.impl.TriggerRequestPayloadFactory;
+import icecube.daq.trigger.control.ITriggerControl;
+import icecube.daq.trigger.component.GlobalConfiguration;
+import icecube.daq.trigger.config.TriggerBuilder;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -38,6 +40,7 @@ import java.nio.channels.Pipe;
 import java.nio.channels.SelectableChannel;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -137,6 +140,7 @@ public class StringHubComponent extends DAQComponent
 	private DataCollectorMonitor collectorMonitor;
 
     private boolean enableTriggering = false;
+    private ISourceID sourceId;
     private IStringTriggerHandler triggerHandler;
 
     public StringHubComponent(int hubId) throws Exception
@@ -173,7 +177,7 @@ public class StringHubComponent extends DAQComponent
         // Check if triggering is enabled
         IPayloadDestinationCollection hitColl;
         if (enableTriggering) {
-            ISourceID sourceId = SourceIdRegistry.getISourceIDFromNameAndId(COMPONENT_NAME, hubId);
+            sourceId = SourceIdRegistry.getISourceIDFromNameAndId(COMPONENT_NAME, hubId);
             triggerHandler = new StringTriggerHandler(sourceId);
             triggerHandler.setMasterPayloadFactory(payloadFactory);
             triggerHandler.setPayloadDestinationCollection(hitOut.getPayloadDestinationCollection());
@@ -399,8 +403,14 @@ public class StringHubComponent extends DAQComponent
 			e.printStackTrace();
 			throw new DAQCompException(e.getMessage());
 		}
-		
-	}
+
+
+        // If triggers are enabled, configure them
+        if (enableTriggering) {
+            configureTrigger(configName);
+        }
+
+    }
 
 	/**
 	 * Controller wants stringhub to start sending data.  Tell DOMs to start up.
@@ -495,5 +505,27 @@ public class StringHubComponent extends DAQComponent
 
         logger.info("Returning from stop.");
 	}
+
+    private void configureTrigger(String configName) throws DAQCompException {
+        // Lookup the trigger configuration
+        String triggerConfiguration;
+        String globalConfigurationFileName = configurationPath + "/" + configName + ".xml";
+        try {
+            triggerConfiguration = GlobalConfiguration.getTriggerConfig(globalConfigurationFileName);
+        } catch (Exception e) {
+            logger.error("Error extracting trigger configuration name from global configuraion file.", e);
+            throw new DAQCompException("Cannot get trigger configuration name.", e);
+        }
+        String triggerConfigFileName = configurationPath + "/trigger/" + triggerConfiguration + ".xml";
+
+        // Add triggers to the trigger manager
+        List currentTriggers = TriggerBuilder.buildTriggers(triggerConfigFileName, sourceId);
+        Iterator triggerIter = currentTriggers.iterator();
+        while (triggerIter.hasNext()) {
+            ITriggerControl trigger = (ITriggerControl) triggerIter.next();
+            trigger.setTriggerHandler(triggerHandler);
+        }
+        triggerHandler.addTriggers(currentTriggers);
+    }
 
 }
