241c1e4887ac6db0a3efa7879b00851d5ef220dc
Kael Hanson <kael.hanson@gmail.com>
2007-12-05 09:53:41 -0600

    nc

--- src/main/java/icecube/daq/dor/TCALTest.java
+++ src/main/java/icecube/daq/dor/TCALTest.java
@@ -6,8 +6,13 @@ package icecube.daq.dor;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 import icecube.daq.rapcal.LeadingEdgeRAPCal;
 import icecube.daq.rapcal.RAPCal;
+import icecube.daq.util.UTC;
 
 public class TCALTest
 {
@@ -17,45 +22,67 @@ public class TCALTest
     private Driver driver = Driver.getInstance();
     private RAPCal rapcal;
     
-    private TCALTest(int card, int pair, char dom)
+    private TCALTest(int card, int pair, char dom, double thresh)
     {
         this.card = card;
         this.pair = pair;
         this.dom  = dom;
-        rapcal  = new LeadingEdgeRAPCal(50.0);
+        rapcal  = new LeadingEdgeRAPCal(thresh);
     }
 
     private void run(int n) throws Exception
     {
         ArrayList<Double> cableLengthList = new ArrayList<Double>();
-        rapcal.update(driver.readTCAL(card, pair, dom), driver.readGPS(card).getOffset());
+        UTC u0 = new UTC();
+        rapcal.update(driver.readTCAL(card, pair, dom), u0);
         for (int iter = 0; iter < n; iter++)
         {
-            GPSInfo gps = driver.readGPS(card);
             TimeCalib tcal = driver.readTCAL(card, pair, dom);
-            rapcal.update(tcal, gps.getOffset());
+            rapcal.update(tcal, u0);
             System.out.println(
-                    tcal.getDorTx() + " " + gps.getOffset() + " " +  
                     rapcal.cableLength() + " " +
                     rapcal.clockRatio());
             cableLengthList.add(rapcal.cableLength());
         }
-        
-
+        double mean = 0.0;
+        for (double x : cableLengthList) mean += x;
+        mean /= cableLengthList.size();
+        double var  = 0.0;
+        for (double x : cableLengthList) 
+        {
+            double svar = x - mean;
+            var += svar * svar;
+        }
+        var /= cableLengthList.size();
+        System.out.format("CLEN: %.1f +/- %.1f\n", mean * 1.0E+09, Math.sqrt(var) * 1.0E+09);
     }
+    
     public static void main(String[] args) throws Exception
     {
+        double threshold = 50.0;
+        BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.INFO);
         if (args.length < 2)
         {
-            System.err.println("usage - java icecube.daq.dor.TCALTest [cwd] [# iter]");
+            System.err.println("usage - java icecube.daq.dor.TCALTest [opts] [cwd] [# iter]");
             System.exit(1);
         }
-        int card = Integer.parseInt(args[0].substring(0, 1));
-        int pair = Integer.parseInt(args[0].substring(1, 2));
-        char dom = args[0].substring(2).toUpperCase().charAt(0);
-        int nIter = Integer.parseInt(args[1]);
+        int iarg = 0;
+        while (args[iarg].charAt(0) == '-')
+        {
+            String opt = args[iarg++].substring(1);
+            if (opt.equals("thresh"))
+            {
+                threshold = Double.parseDouble(args[iarg++]);
+            }
+        }
         
-        TCALTest test = new TCALTest(card, pair, dom);
+        int card = Integer.parseInt(args[iarg].substring(0, 1));
+        int pair = Integer.parseInt(args[iarg].substring(1, 2));
+        char dom = args[iarg++].substring(2).toUpperCase().charAt(0);
+        int nIter = Integer.parseInt(args[iarg++]);
+        
+        TCALTest test = new TCALTest(card, pair, dom, threshold);
         
         test.run(nIter);
     }
