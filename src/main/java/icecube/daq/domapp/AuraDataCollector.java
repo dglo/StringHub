b23f544ad9e5a8397ca0483dc43d9daee6dbb211
Kael Hanson <kael.hanson@gmail.com>
2009-12-03 20:42:49 -0600

    this check-in should simply be formatting changes

--- src/main/java/icecube/daq/domapp/AuraDataCollector.java
+++ src/main/java/icecube/daq/domapp/AuraDataCollector.java
@@ -22,41 +22,42 @@ import org.apache.log4j.Logger;
 
 public class AuraDataCollector extends AbstractDataCollector
 {
-    private AuraDRM drm;
-    private Driver driver;
-    private RAPCal rapcal;
-    private BufferConsumer hits;
-    private AtomicBoolean running;
-    private AtomicBoolean forcedTrigger;
-    private AtomicBoolean radioTrigger;
-    private AtomicBoolean changeTriggerSetting;
-    private int powerControlBits;
-    private boolean useDOMApp;
-    private String mbid;
-    private long mbid_numerique;
-    private int evtCnt;
-    private long tracr_clock_offset = 0L;
+    private AuraDRM             drm;
+    private Driver              driver;
+    private RAPCal              rapcal;
+    private BufferConsumer      hits;
+    private AtomicBoolean       running;
+    private AtomicBoolean       forcedTrigger;
+    private AtomicBoolean       radioTrigger;
+    private AtomicBoolean       changeTriggerSetting;
+    private int                 powerControlBits;
+    private boolean             useDOMApp;
+    private String              mbid;
+    private long                mbid_numerique;
+    private int                 evtCnt;
+    private long                tracr_clock_offset  = 0L;
 
-    private static final int EVT_DOM_CLK         = 28;
-    private static final int EVT_TRACR_MATCH_CLK = 40;
-    private static final int EVT_TRACR_CLK       = 106;
+    private static final int    EVT_DOM_CLK         = 28;
+    private static final int    EVT_TRACR_MATCH_CLK = 40;
+    private static final int    EVT_TRACR_CLK       = 106;
 
-    private short[][] radioDACs = {
+    private short[][]           radioDACs           = { 
             { 2000, 3000, 3200, 3250 },
-            { 2000, 3000, 3200, 3250 },
-            { 2000, 3000, 3200, 3250 },
-            { 2000, 3000, 3200, 3250 }
-    };
-    
-    private int triggerSetting=68; // This is the default 3/4 channels and 3/4 bands
-    private static final Logger logger = Logger.getLogger(AuraDataCollector.class);
-    
+            { 2000, 3000, 3200, 3250 }, 
+            { 2000, 3000, 3200, 3250 }, 
+            { 2000, 3000, 3200, 3250 } };
+
+    // This is the default : 3/4 ch and 3/4 bands
+    private int                 triggerSetting      = 68;   
+    private static final Logger logger              = Logger.getLogger(AuraDataCollector.class);
+
     public AuraDataCollector(int card, int pair, char dom, BufferConsumer hits)
     {
         this(card, pair, dom, hits, true);
     }
-    
-    public AuraDataCollector(int card, int pair, char dom, BufferConsumer hits, boolean useDOMApp)
+
+    public AuraDataCollector(int card, int pair, char dom, BufferConsumer hits,
+            boolean useDOMApp)
     {
         super(card, pair, dom);
         driver = Driver.getInstance();
@@ -64,13 +65,13 @@ public class AuraDataCollector extends AbstractDataCollector
         this.hits = hits;
         this.evtCnt = 0;
         running = new AtomicBoolean(false);
-        //this.powerControlBits = 0x3f;
+        // this.powerControlBits = 0x3f;
         this.useDOMApp = useDOMApp;
         this.forcedTrigger = new AtomicBoolean(true);
-        this.radioTrigger  = new AtomicBoolean(false);
-	changeTriggerSetting = new AtomicBoolean(false);
+        this.radioTrigger = new AtomicBoolean(false);
+        changeTriggerSetting = new AtomicBoolean(false);
     }
-    
+
     public void run()
     {
         Timer t3 = new Timer();
@@ -81,7 +82,7 @@ public class AuraDataCollector extends AbstractDataCollector
             drm = new AuraDRM(card, pair, dom);
             mbid = drm.getMainboardId();
             mbid_numerique = Long.parseLong(mbid, 16);
-            
+
             if (useDOMApp) drm.loadDOMAppSBI();
 
             // Now flag this process as IDLE
@@ -95,61 +96,65 @@ public class AuraDataCollector extends AbstractDataCollector
                 switch (getRunLevel())
                 {
                 case CONFIGURING:
-		    // Turn on TRACR 
+                    // Turn on TRACR
                     Thread.sleep(1000);
                     drm.powerOnFlasherboard();
                     Thread.sleep(5500);
-		    /*
-		     * Get tracr-mb time offset. Make sure the cluster does not trigger by setting all DACs to 0
-		     */
+                    /*
+                     * Get tracr-mb time offset. Make sure the cluster does not
+                     * trigger by setting all DACs to 0
+                     */
 
-		    for (int ant = 0; ant < 4; ant++)
-			for (int band = 0; band < 4; band++)
-			    drm.setRadioDAC(ant, band, 0);
-		    drm.writeRadioDACs();
-		    drm.resetTRACRFifo();
+                    for (int ant = 0; ant < 4; ant++)
+                        for (int band = 0; band < 4; band++)
+                            drm.setRadioDAC(ant, band, 0);
+                    drm.writeRadioDACs();
+                    drm.resetTRACRFifo();
                     tracr_clock_offset = drm.getTRACRClockOffset(10000);
                     logger.info(" Got TRACR offset: " + tracr_clock_offset);
-  
-                    /* 
-		     * Turn on power
-                     * There is a little black-magic here -- amplifier bits and then 
-                     * SHORTS are turned on one by one.
+
+                    /*
+                     * Turn on power There is a little black-magic here --
+                     * amplifier bits and then SHORTS are turned on one by one.
                      */
-                    if ( !(drm.writePowerBits(powerControlBits)))
+                    if (!(drm.writePowerBits(powerControlBits)))
                     {
                         setRunLevel(RunLevel.STOPPING);
                     }
                     else
-                    { 
-			if (radioTrigger.get())  // Write dacs only if not forced trigger
-			    {
-				for (int ant = 0; ant < 4; ant++)
-				    for (int band = 0; band < 4; band++)
-					drm.setRadioDAC(ant, band, radioDACs[ant][band]);
-				drm.writeRadioDACs();
-			    }
-			if (changeTriggerSetting.get()) {
-			    drm.setTriggerLogic(triggerSetting);
-			    logger.info(mbid+" Trigger Setting changed to"+drm.getTriggerLogic());
-			}
-			else
-			{
-			    logger.info(mbid+" Default trigger Setting used"+drm.getTriggerLogic());
-			}  
+                    {
+                        if (radioTrigger.get()) // Write dacs only if not forced
+                                                // trigger
+                        {
+                            for (int ant = 0; ant < 4; ant++)
+                                for (int band = 0; band < 4; band++)
+                                    drm.setRadioDAC(ant, band, radioDACs[ant][band]);
+                            drm.writeRadioDACs();
+                        }
+                        if (changeTriggerSetting.get())
+                        {
+                            drm.setTriggerLogic(triggerSetting);
+                            logger.info(mbid + " Trigger Setting changed to"
+                                    + drm.getTriggerLogic());
+                        }
+                        else
+                        {
+                            logger.info(mbid + " Default trigger Setting used"
+                                    + drm.getTriggerLogic());
+                        }
 
                         setRunLevel(RunLevel.CONFIGURED);
                     }
 
                     break;
                 case STARTING:
-		    drm.resetTRACRFifo();
-                    logger.info("STARTING RUN on DOM " + mbid );
+                    drm.resetTRACRFifo();
+                    logger.info("STARTING RUN on DOM " + mbid);
                     setRunLevel(RunLevel.RUNNING);
                     break;
                 case RUNNING:
                     if (forcedTrigger.get()) sendRadioBuffer(drm.forcedTrig(1));
-                    if (radioTrigger.get()) sendRadioBuffer(drm.radioTrig(1)); 
+                    if (radioTrigger.get()) sendRadioBuffer(drm.radioTrig(1));
                     break;
                 case STOPPING:
                     drm.powerOffFlasherboard();
@@ -157,7 +162,7 @@ public class AuraDataCollector extends AbstractDataCollector
                     break;
                 }
             }
-        
+
         }
         catch (Exception ex)
         {
@@ -176,28 +181,31 @@ public class AuraDataCollector extends AbstractDataCollector
 
     private void sendRadioBuffer(ByteBuffer buf)
     {
-        int xtbSize = buf.limit()+32;
+        int xtbSize = buf.limit() + 32;
 
         // Decode clocks and translate to UT via rapcal
         long utc;
         buf.order(ByteOrder.LITTLE_ENDIAN);
         long domclk = buf.getLong(EVT_DOM_CLK);
-        if (buf.limit() > EVT_TRACR_CLK+6) {
+        if (buf.limit() > EVT_TRACR_CLK + 6)
+        {
             long tracr_match_clk = buf.getLong(EVT_TRACR_MATCH_CLK) & 0xffffffffffL;
             long tracr_clk = 0;
             buf.order(ByteOrder.BIG_ENDIAN);
             for (int i = 0; i < 6; i++)
-                tracr_clk = (tracr_clk << 8) | ((int)buf.get(EVT_TRACR_CLK+i) & 0xff);
+                tracr_clk = (tracr_clk << 8) | ((int) buf.get(EVT_TRACR_CLK + i) & 0xff);
             tracr_clk = tracr_clk & 0xffffffffffL;
             buf.order(ByteOrder.LITTLE_ENDIAN);
-            utc = rapcal.domToUTC(2*tracr_clk + tracr_clock_offset).in_0_1ns();
-            if (logger.isDebugEnabled()) {
-                logger.debug("DOMClk: " + domclk + " TracrMatchClk: "+tracr_match_clk
-                             +" TracrClk: "+tracr_clk+" - UTC: " + utc); 
-                logger.debug("Offset (.1ns): " + (rapcal.domToUTC(domclk).in_0_1ns()-utc));
+            utc = rapcal.domToUTC(2 * tracr_clk + tracr_clock_offset).in_0_1ns();
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("DOMClk: " + domclk + " TracrMatchClk: " + tracr_match_clk
+                        + " TracrClk: " + tracr_clk + " - UTC: " + utc);
+                logger.debug("Offset (.1ns): " + (rapcal.domToUTC(domclk).in_0_1ns() - utc));
             }
         }
-        else {            
+        else
+        {
             utc = rapcal.domToUTC(domclk).in_0_1ns();
             if (logger.isDebugEnabled())
                 logger.debug("No tracr clock (beacon?): DOMClk: " + domclk + " - UTC: " + utc);
@@ -221,31 +229,33 @@ public class AuraDataCollector extends AbstractDataCollector
             logger.error("Caught IOException: " + iox.getLocalizedMessage());
         }
     }
-    
+
     public String getMainboardId()
     {
         return this.mbid;
     }
-    
-    public void setPowerLevel (int value)
+
+    public void setPowerLevel(int value)
     {
-	this.powerControlBits=value;
+        this.powerControlBits = value;
     }
+
     public void setTriggerLogic(int value)
     {
-	this.triggerSetting = value;
-	changeTriggerSetting.set(true);
-    } 
+        this.triggerSetting = value;
+        changeTriggerSetting.set(true);
+    }
+
     public void setForcedTriggers(boolean enabled)
     {
         forcedTrigger.set(enabled);
     }
-    
+
     public void setRadioTriggers(boolean enabled)
     {
         radioTrigger.set(enabled);
     }
-    
+
     public void setRadioDACs(short[][] dacs)
     {
         this.radioDACs = dacs;
@@ -293,16 +303,16 @@ public class AuraDataCollector extends AbstractDataCollector
         running.set(false);
 
     }
-    
+
     private class TCALTask extends TimerTask
     {
         private UTC gpsOffset;
-        
+
         TCALTask()
         {
             gpsOffset = new UTC();
         }
-        
+
         @Override
         public void run()
         {
@@ -335,9 +345,9 @@ public class AuraDataCollector extends AbstractDataCollector
             {
                 rcx.printStackTrace();
             }
-            
+
         }
-        
+
     }
 
 }
