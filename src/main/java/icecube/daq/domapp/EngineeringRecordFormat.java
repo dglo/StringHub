3731cbdbe0571eb77c024a32a209d2dcb7eaaee0
IceCube Intern <intern@icecube.wisc.edu>
2011-08-26 08:35:13 -0500

    commiting format changes in StringHub files

--- src/main/java/icecube/daq/domapp/EngineeringRecordFormat.java
+++ src/main/java/icecube/daq/domapp/EngineeringRecordFormat.java
@@ -2,165 +2,209 @@ package icecube.daq.domapp;
 
 import java.io.Serializable;
 
-public class EngineeringRecordFormat implements Serializable {
-	private static final long serialVersionUID = 1L;
-	private short fadc_samples;
-	private short[] atwd_size;
-	private short[] atwd_samples;
-	private transient byte[] enc;
+public class EngineeringRecordFormat implements Serializable 
+{
+    private static final long serialVersionUID = 1L;
+    private short fadc_samples;
+    private short[] atwd_size;
+    private short[] atwd_samples;
+    private transient byte[] enc;
 
-	public EngineeringRecordFormat(short fadc_samples, short[] atwd_samples, short[] atwd_size) throws BadEngineeringFormat {
+    public EngineeringRecordFormat(short fadc_samples, short[] atwd_samples, 
+        short[] atwd_size) throws BadEngineeringFormat 
+    {
 
-		this.fadc_samples = fadc_samples;
-		if (atwd_samples == null) {
-			this.atwd_samples = new short[4];
-			this.atwd_samples[0] = 128;
-			this.atwd_samples[1] = 128;
-			this.atwd_samples[2] = 128;
-			this.atwd_samples[3] = 0;
-		} else {
-			this.atwd_samples = atwd_samples;
-		}
-		if (atwd_size == null) {
-			this.atwd_size = new short[4];
-			this.atwd_size[0] = 2;
-			this.atwd_size[1] = 2;
-			this.atwd_size[2] = 2;
-			this.atwd_size[3] = 2;
-		} else {
-			this.atwd_size = atwd_size;
-		}
+        this.fadc_samples = fadc_samples;
+        if (atwd_samples == null) {
+            this.atwd_samples = new short[4];
+            this.atwd_samples[0] = 128;
+            this.atwd_samples[1] = 128;
+            this.atwd_samples[2] = 128;
+            this.atwd_samples[3] = 0;
+        } else {
+            this.atwd_samples = atwd_samples;
+        }
+        if (atwd_size == null) {
+            this.atwd_size = new short[4];
+            this.atwd_size[0] = 2;
+            this.atwd_size[1] = 2;
+            this.atwd_size[2] = 2;
+            this.atwd_size[3] = 2;
+        } else {
+            this.atwd_size = atwd_size;
+        }
 
-		_int_enc();
-	}
+        _int_enc();
+    }
 
-	public EngineeringRecordFormat(short fadc_samples, short[] atwd_samples) throws BadEngineeringFormat {
-		this(fadc_samples, atwd_samples, null);
-	}
+    public EngineeringRecordFormat(short fadc_samples, short[] atwd_samples) 
+        throws BadEngineeringFormat 
+    {
+        this(fadc_samples, atwd_samples, null);
+    }
 
-	public EngineeringRecordFormat(short fadc_samples) throws BadEngineeringFormat {
-		this(fadc_samples, null);
-	}
+    public EngineeringRecordFormat(short fadc_samples) 
+        throws BadEngineeringFormat 
+    {
+        this(fadc_samples, null);
+    }
 
-	public EngineeringRecordFormat() {
-		this.atwd_samples = new short[4];
-		this.atwd_size    = new short[4];
-		this.fadc_samples = 255;
-		atwd_samples[0] = atwd_samples[1] = atwd_samples[2] = 128;
-		atwd_samples[3] = 0;
-		atwd_size[0] = atwd_size[1] = atwd_size[2] = atwd_size[3] = 2;
-		enc = new byte[3];
-		enc[0] = -1;
-		enc[1] = -1;
-		enc[2] = 15;
-	}
+    public EngineeringRecordFormat() 
+    {
+        this.atwd_samples = new short[4];
+        this.atwd_size    = new short[4];
+        this.fadc_samples = 255;
+        atwd_samples[0] = 128;
+        atwd_samples[1] = 128;
+        atwd_samples[2] = 128;
+        atwd_samples[3] = 0;
+        atwd_size[0] = 2;
+        atwd_size[1] = 2;
+        atwd_size[2] = 2;
+        atwd_size[3] = 2;
+        enc = new byte[3];
+        enc[0] = -1;
+        enc[1] = -1;
+        enc[2] = 15;
+    }
 
-	/**
-	 * The copy constructor will create a new object initialized with
-	 * values from the supplied object
-	 * @param copyFrom the object whose values will be copied into the new instance
-	 */
-	public EngineeringRecordFormat(EngineeringRecordFormat copyFrom)
-	{
-		fadc_samples = copyFrom.fadc_samples;
-		atwd_samples = new short[4];
-		atwd_size    = new short[4];
-		enc          = new byte[3];
-		System.arraycopy(copyFrom.atwd_samples, 0, atwd_samples, 0, 4);
-		System.arraycopy(copyFrom.atwd_size, 0, atwd_size, 0, 4);
-		System.arraycopy(copyFrom.enc, 0, enc, 0, 3);
-	}
+    /**
+     * The copy constructor will create a new object initialized with
+     * values from the supplied object
+     * @param copyFrom the object whose values will be copied into 
+     *  the new instance
+     */
+    public EngineeringRecordFormat(EngineeringRecordFormat copyFrom)
+    {
+        fadc_samples = copyFrom.fadc_samples;
+        atwd_samples = new short[4];
+        atwd_size    = new short[4];
+        enc          = new byte[3];
+        System.arraycopy(copyFrom.atwd_samples, 0, atwd_samples, 0, 4);
+        System.arraycopy(copyFrom.atwd_size, 0, atwd_size, 0, 4);
+        System.arraycopy(copyFrom.enc, 0, enc, 0, 3);
+    }
 
-	/**
-	 * Decode the byte triplet to find engineering format
-	 * @param b0 byte #5 of engineering record
-	 * @param b1 byte #6 of engineering record
-	 * @param b2 byte #7 of engineering record
-	 */
-	public EngineeringRecordFormat(byte b0, byte b1, byte b2) {
+    /**
+     * Decode the byte triplet to find engineering format
+     * @param b0 byte #5 of engineering record
+     * @param b1 byte #6 of engineering record
+     * @param b2 byte #7 of engineering record
+     */
+    public EngineeringRecordFormat(byte b0, byte b1, byte b2) 
+    {
 
-		atwd_samples = new short[4];
-		atwd_size = new short[4];
+        atwd_samples = new short[4];
+        atwd_size = new short[4];
 
-		fadc_samples = b0;
-		if (fadc_samples < 0) fadc_samples += 256;
+        fadc_samples = b0;
+        if (fadc_samples < 0) {
+            fadc_samples += 256;
+        }
 
-		// decode the nybbles
-		int[] n = new int[4];
-		n[0] = b1 & 15;
-		n[1] = (b1 & 0xf0) >> 4;
-		n[2] = b2 & 15;
-		n[3] = (b2 & 0xf0) >> 4;
-		for (int ch = 0; ch < 4; ch++) {
-			if ((n[ch] & 2) != 0)
-				atwd_size[ch] = 2;
-			else
-				atwd_size[ch] = 1;
-			if ((n[ch] & 1) != 0) {
-				switch ((n[ch] & 12) >> 2) {
-				case 0: atwd_samples[ch] = 32; break;
-				case 1: atwd_samples[ch] = 64; break;
-				case 2: atwd_samples[ch] = 16; break;
-				case 3: atwd_samples[ch] = 128; break;
-				}
-			} else
-				atwd_samples[ch] = 0;
-		}
-	}
+        // decode the nybbles
+        int[] n = new int[4];
+        n[0] = b1 & 15;
+        n[1] = (b1 & 0xf0) >> 4;
+        n[2] = b2 & 15;
+        n[3] = (b2 & 0xf0) >> 4;
+        for (int ch = 0; ch < 4; ch++) {
+            if ((n[ch] & 2) != 0) {
+                atwd_size[ch] = 2;
+            } else {
+                atwd_size[ch] = 1;
+            }
+            if ((n[ch] & 1) != 0) {
+                switch ((n[ch] & 12) >> 2) {
+                case 0: 
+                    atwd_samples[ch] = 32; 
+                    break;
+                case 1: 
+                    atwd_samples[ch] = 64; 
+                    break;
+                case 2: 
+                    atwd_samples[ch] = 16; 
+                    break;
+                case 3: 
+                    atwd_samples[ch] = 128; 
+                    break;
+                default:
+                    break;
+                }
+            } else {
+                atwd_samples[ch] = 0;
+            }
+        }
+    }
 
-	public short fadcSamples() {
-		return fadc_samples;
-	}
+    public short fadcSamples() 
+    {
+        return fadc_samples;
+    }
 
-	public void setAtwdSamples(int ch, short val) throws BadEngineeringFormat
-	{
-		atwd_samples[ch] = val;
-		_int_enc();
-	}
+    public void setAtwdSamples(int ch, short val) throws BadEngineeringFormat
+    {
+        atwd_samples[ch] = val;
+        _int_enc();
+    }
 
-	public void setAtwdWordsize(int ch, short val) throws BadEngineeringFormat
-	{
-		atwd_size[ch] = val;
-		_int_enc();
-	}
+    public void setAtwdWordsize(int ch, short val) throws BadEngineeringFormat
+    {
+        atwd_size[ch] = val;
+        _int_enc();
+    }
 
-	public void setFadcSamples(short val) throws BadEngineeringFormat
-	{
-		fadc_samples = val;
-	}
+    public void setFadcSamples(short val) throws BadEngineeringFormat
+    {
+        fadc_samples = val;
+    }
 
-	public short atwdSamples(int ch) {
-		return atwd_samples[ch];
-	}
+    public short atwdSamples(int ch) 
+    {
+        return atwd_samples[ch];
+    }
 
-	public short atwdWordsize(int ch) {
-		return atwd_size[ch];
-	}
+    public short atwdWordsize(int ch) 
+    {
+        return atwd_size[ch];
+    }
 
-	public byte[] encode() {
-		return enc;
-	}
+    public byte[] encode() 
+    {
+        return enc;
+    }
 
-	private void _int_enc() throws BadEngineeringFormat {
-		enc = new byte[3];
-		enc[0] = (byte) fadc_samples;
-		enc[1] = (byte) (atwdPackedFormat(atwd_size[0], atwd_samples[0]) |
-				(atwdPackedFormat(atwd_size[1], atwd_samples[1]) << 4));
-		enc[2] = (byte) (atwdPackedFormat(atwd_size[2], atwd_samples[2]) |
-				(atwdPackedFormat(atwd_size[3], atwd_samples[3]) << 4));
-	}
+    private void _int_enc() throws BadEngineeringFormat 
+    {
+        enc = new byte[3];
+        enc[0] = (byte) fadc_samples;
+        enc[1] = (byte) (atwdPackedFormat(atwd_size[0], atwd_samples[0]) |
+                (atwdPackedFormat(atwd_size[1], atwd_samples[1]) << 4));
+        enc[2] = (byte) (atwdPackedFormat(atwd_size[2], atwd_samples[2]) |
+                (atwdPackedFormat(atwd_size[3], atwd_samples[3]) << 4));
+    }
 
-	private int atwdPackedFormat(int size, int samples) throws BadEngineeringFormat {
-		int nybble = 1;
-		if (size == 2) nybble = 3;
-		switch (samples) {
-		case 0: 	return 0;
-		case 16: 	return nybble | 8;
-		case 32: 	return nybble;
-		case 64: 	return nybble | 4;
-		case 128: 	return nybble | 12;
-		default: 	throw new BadEngineeringFormat(size, samples);
-		}
-	}
+    private int atwdPackedFormat(int size, int samples) 
+        throws BadEngineeringFormat 
+    {
+        int nybble = 1;
+        if (size == 2) {
+            nybble = 3;
+        }
+        switch (samples) {
+        case 0:    
+            return 0;
+        case 16:     
+            return nybble | 8;
+        case 32:    
+            return nybble;
+        case 64:     
+            return nybble | 4;
+        case 128:     
+            return nybble | 12;
+        default:    
+            throw new BadEngineeringFormat(size, samples);
+        }
+    }
 
 }
--- src/main/java/icecube/daq/domapp/MessageType.java
+++ src/main/java/icecube/daq/domapp/MessageType.java
@@ -5,144 +5,153 @@ package icecube.daq.domapp;
  * the StringHub and the DOMApp running on a DOM under its control.
  * The DOMApp messages have a <b>TYPE</b> and <b>SUBTYPE</b>
  * which are identified in the enum constructor.
- * @see John Jacobsen's <a href="http://www.npxdesigns.com/domapp/api.html">DOMApp API</a>
+ * @see John Jacobsen's 
+ * <a href="http://www.npxdesigns.com/domapp/api.html">DOMApp API</a>
  * @author kael dylan hanson
  *
  */
 public enum MessageType {
 
     /** Request the DOM's 12-character hex mainboard ID */
-	GET_DOM_ID(1, 10),
-	
-	/** Get the DOMApp release information returned as a string */
-	GET_DOMAPP_RELEASE(1, 24),
-	
-	WRITE_DAC(2, 13),
-	SET_HV(2, 14),
-	ENABLE_HV(2, 16),
-	DISABLE_HV(2, 18),
-	QUERY_HV(2, 22),
-	SET_TRIG_MODE(2, 31),
-	MUX_SELECT(2, 35),
-	GET_MUX_CH(2, 36),
-	SET_PULSER_RATE(2, 37),
-	GET_PULSER_RATE(2, 38),
-	SET_PULSER_ON(2, 39),
-	SET_PULSER_OFF(2, 40),
-	SET_SCALER_DEADTIME(2, 43),
-	GET_SCALER_DEADTIME(2, 44),
-	SET_LC_MODE(2, 45),
-	GET_LC_MODE(2, 46),
-	SET_LC_WIN(2, 47),
-	GET_LC_WIN(2, 48),
-	SET_LC_TYPE(2, 49),
-	GET_LC_TYPE(2, 50),
-	SET_LC_TX(2, 51),
-	GET_LC_TX(2, 52),
-	SET_LC_SRC(2, 53),
-	GET_LC_SRC(2, 54),
-	SET_LC_SPAN(2, 55),
-	GET_LC_SPAN(2, 56),
-	SET_LC_CABLELEN(2, 57),
-	GET_LC_CABLELEN(2, 58),
-	ENABLE_SN(2, 59),
-	DISABLE_SN(2, 60),
-	
-	/** Set charge stamp type for delta-compressed events. If mode 
-	 * is 0 (default), the FADC will be used. If mode is 1, the ATWD
-	 * is used. If ATWD, chSel determines whether channel selection 
-	 * is auto (0), or specified in the third argument (1). Third 
-	 * argument must specify channel 0 or 1.
-	 */
-	SET_CHARGE_STAMP_TYPE(2, 61),
-	
-	/** Enable (1) or disable (0) transmission of MinBias waveforms (1:8192 prescale) */
-	SELECT_MINBIAS(2,62),
-	
-	GET_DATA_ACCESS_ERROR_ID(2, 2),
-	GET_DATA_ACCESS_ERROR_STR(2, 3),
-	GET_DATA(3, 11),
-	
-	GET_MONI(3, 12),
-	SET_MONI_IVAL(3, 13),
-	SET_ENG_FORMAT(3, 14),
-	GET_FBID(3, 23),
-	
-	SET_DATA_FORMAT(3, 24),
-	GET_DATA_FORMAT(3, 25),
-	
-	SET_COMPRESSION_MODE(3, 26),
-	GET_COMPRESSION_MODE(3, 27),
-	
-	GET_SN_DATA(3, 28),
-	RESET_MONI_BUF(3, 29),
-	QUERY_MONI_AVAIL(3, 30),
-	QUERY_OVERFLOWS(3, 31),
-	
-	SET_LBM_DEPTH(3, 32),
-	GET_LBM_DEPTH(3, 33),
-	
-	/**
-	 * Generate charge stamp histograms in monitoring stream, at 
-	 * <tt>interval</tt> clock ticks (if &ge;40,000,000) or seconds 
-	 * (if &lt;40,000,000). If interval is zero (default), no records 
-	 * will be generated. Prescale is the divisor of each charge stamp 
-	 * amplitude before it is histogrammed. DSC_SET_CHARGE_STAMP_TYPE 
-	 * is used to select the type of charge stamp; if FADC (default), 
-	 * only the peak value of the charge stamp is histogrammed.  
-	 * <p>
-	 * The histograms will be generated in the monitoring stream in 
-	 * ASCII format, as follows:
-	 * <pre>
-	 * ATWD CS chip chan--N entries: bin0 bin1 ... bin127
-	 * </pre>
-	 * (four entries per interval, where chip is A or B and chan is 0 or 1)
-	 * or
-	 * <pre>
-	 * FADC CS--N entries: bin0 bin1 ... bin127
-	 * </pre>
-	 */
-	HISTO_CHARGE_STAMPS(3, 34),
-	
-	
-	SELECT_ATWD(3, 35),
-	
-	GET_FAST_MONI_RATE_TYPE(3, 36),
-	SET_FAST_MONI_RATE_TYPE(3, 37),
-	
-	/** Start a data collection run on the DOM */
-	BEGIN_RUN(4, 12),
-	
-	/** Stop a data run on the DOM */
-	END_RUN(4,13),
-	
-	/** Execute a pedestal run on the DOM and store pedestals
-	 * to be subtracted from waveforms in subsequent data run.
-	 */
-	COLLECT_PEDESTALS(4, 16),
-	
-	/** Begin a flasher run */
-	BEGIN_FB_RUN(4, 27),
-	
-	/** Stop an on-going flasher run */
-	END_FB_RUN(4, 28),
-	
-	/** Change FB parameters - akin to the BEGIN_FB_RUN message */
-	CHANGE_FB_SETTINGS(4, 29);
+    GET_DOM_ID(1, 10),
+    
+    /** Get the DOMApp release information returned as a string */
+    GET_DOMAPP_RELEASE(1, 24),
+    
+    WRITE_DAC(2, 13),
+    SET_HV(2, 14),
+    ENABLE_HV(2, 16),
+    DISABLE_HV(2, 18),
+    QUERY_HV(2, 22),
+    SET_TRIG_MODE(2, 31),
+    MUX_SELECT(2, 35),
+    GET_MUX_CH(2, 36),
+    SET_PULSER_RATE(2, 37),
+    GET_PULSER_RATE(2, 38),
+    SET_PULSER_ON(2, 39),
+    SET_PULSER_OFF(2, 40),
+    SET_SCALER_DEADTIME(2, 43),
+    GET_SCALER_DEADTIME(2, 44),
+    SET_LC_MODE(2, 45),
+    GET_LC_MODE(2, 46),
+    SET_LC_WIN(2, 47),
+    GET_LC_WIN(2, 48),
+    SET_LC_TYPE(2, 49),
+    GET_LC_TYPE(2, 50),
+    SET_LC_TX(2, 51),
+    GET_LC_TX(2, 52),
+    SET_LC_SRC(2, 53),
+    GET_LC_SRC(2, 54),
+    SET_LC_SPAN(2, 55),
+    GET_LC_SPAN(2, 56),
+    SET_LC_CABLELEN(2, 57),
+    GET_LC_CABLELEN(2, 58),
+    ENABLE_SN(2, 59),
+    DISABLE_SN(2, 60),
+    
+    /** Set charge stamp type for delta-compressed events. If mode 
+     * is 0 (default), the FADC will be used. If mode is 1, the ATWD
+     * is used. If ATWD, chSel determines whether channel selection 
+     * is auto (0), or specified in the third argument (1). Third 
+     * argument must specify channel 0 or 1.
+     */
+    SET_CHARGE_STAMP_TYPE(2, 61),
+    
+    /** Enable (1) or disable (0) transmission of MinBias 
+     * waveforms (1:8192 prescale) */
+    SELECT_MINBIAS(2, 62), 
+    
+    GET_DATA_ACCESS_ERROR_ID(2, 2),
+    GET_DATA_ACCESS_ERROR_STR(2, 3),
+    GET_DATA(3, 11),
+    
+    GET_MONI(3, 12),
+    SET_MONI_IVAL(3, 13),
+    SET_ENG_FORMAT(3, 14),
+    GET_FBID(3, 23),
+    
+    SET_DATA_FORMAT(3, 24),
+    GET_DATA_FORMAT(3, 25),
+    
+    SET_COMPRESSION_MODE(3, 26),
+    GET_COMPRESSION_MODE(3, 27),
+    
+    GET_SN_DATA(3, 28),
+    RESET_MONI_BUF(3, 29),
+    QUERY_MONI_AVAIL(3, 30),
+    QUERY_OVERFLOWS(3, 31),
+    
+    SET_LBM_DEPTH(3, 32),
+    GET_LBM_DEPTH(3, 33),
+    
+    /**
+     * Generate charge stamp histograms in monitoring stream, at 
+     * <tt>interval</tt> clock ticks (if &ge;40,000,000) or seconds 
+     * (if &lt;40,000,000). If interval is zero (default), no records 
+     * will be generated. Prescale is the divisor of each charge stamp 
+     * amplitude before it is histogrammed. DSC_SET_CHARGE_STAMP_TYPE 
+     * is used to select the type of charge stamp; if FADC (default), 
+     * only the peak value of the charge stamp is histogrammed.  
+     * <p>
+     * The histograms will be generated in the monitoring stream in 
+     * ASCII format, as follows:
+     * <pre>
+     * ATWD CS chip chan--N entries: bin0 bin1 ... bin127
+     * </pre>
+     * (four entries per interval, where chip is A or B and chan is 0 or 1)
+     * or
+     * <pre>
+     * FADC CS--N entries: bin0 bin1 ... bin127
+     * </pre>
+     */
+    HISTO_CHARGE_STAMPS(3, 34),
+    
+    
+    SELECT_ATWD(3, 35),
+    
+    GET_FAST_MONI_RATE_TYPE(3, 36),
+    SET_FAST_MONI_RATE_TYPE(3, 37),
+    
+    /** Start a data collection run on the DOM */
+    BEGIN_RUN(4, 12), 
 
-	private byte facility;
-	private byte subtype;
+    /** Stop a data run on the DOM */
+    END_RUN(4, 13), 
+    
+    /** Execute a pedestal run on the DOM and store pedestals
+     * to be subtracted from waveforms in subsequent data run.
+     */
+    COLLECT_PEDESTALS(4, 16),
+    
+    /** Begin a flasher run */
+    BEGIN_FB_RUN(4, 27),
+    
+    /** Stop an on-going flasher run */
+    END_FB_RUN(4, 28),
+    
+    /** Change FB parameters - akin to the BEGIN_FB_RUN message */
+    CHANGE_FB_SETTINGS(4, 29);
 
-	MessageType(int facility, int subtype) {
-		this.facility = (byte) facility;
-		this.subtype  = (byte) subtype;
-	}
+    private byte facility;
+    private byte subtype;
 
-	public byte getFacility() { return facility; }
-	public byte getSubtype() { return subtype; }
-	public boolean equals(byte type, byte subtype)
-	{
-	    return this.facility == type && this.subtype == subtype;
-	}
+    MessageType(int facility, int subtype) 
+    {
+        this.facility = (byte) facility;
+        this.subtype  = (byte) subtype;
+    }
+
+    public byte getFacility() 
+    { 
+        return facility; 
+    }
+    public byte getSubtype() 
+    { 
+        return subtype; 
+    }
+    public boolean equals(byte type, byte subtype)
+    {
+        return this.facility == type && this.subtype == subtype;
+    }
 
 }
