342e4da02fafdf93a0dd29bc8305ba31b030cbec
Tim Bendfelt <tim.bendfelt@icecube.wisc.edu>
2017-01-06 11:42:40 -0600

    Issue 8028: Add tests cases for delayed channels.

--- src/test/java/icecube/daq/monitoring/BinnedQueueConsumerTest.java
+++ src/test/java/icecube/daq/monitoring/BinnedQueueConsumerTest.java
@@ -3,6 +3,8 @@ package icecube.daq.monitoring;
 import icecube.daq.common.MockAppender;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.BasicConfigurator;
 
@@ -71,6 +73,8 @@ public class BinnedQueueConsumerTest
     {
         private ArrayList<String> names = new ArrayList<String>();
 
+        private ArrayList<MyReport> reports = new ArrayList<MyReport>();
+
         MyConsumer(IRunMonitor parent, long binWidth)
         {
             super(parent, binWidth);
@@ -103,9 +107,48 @@ public class BinnedQueueConsumerTest
         @Override
         public void sendData(long binStart, long binEnd)
         {
+            Map<String, Integer> counts = new HashMap<>();
             for (String name: names) {
                 MyCounter ctr = getExisting(name, binStart, binEnd);
+                if(ctr != null)
+                {
+                    counts.put(name, ctr.getValue());
+                }
             }
+            MyReport myReport = new MyReport(binStart, binEnd, counts);
+            reports.add(myReport);
+        }
+    }
+
+    class MyReport
+    {
+        final long binStart;
+        final long binEnd;
+        final Map<String, Integer> counts;
+
+        MyReport(final long binStart, final long binEnd, final Map<String, Integer> counts)
+        {
+            this.binStart = binStart;
+            this.binEnd = binEnd;
+            this.counts = counts;
+        }
+
+        @Override
+        public String toString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append("BIN-> [").append(binStart).append( " to ");
+            sb.append(binEnd).append("] counts ").append("{");
+            boolean first = true;
+            for (Map.Entry<String, Integer> entry: counts.entrySet())
+            {
+                sb.append(first ? "" : ", ");
+                sb.append(entry.getKey()).append(": ");
+                sb.append(entry.getValue());
+                first = false;
+            }
+            sb.append("}");
+            return sb.toString();
         }
     }
 
@@ -157,4 +200,187 @@ public class BinnedQueueConsumerTest
             }
         }
     }
+
+
+    @Test
+    public void testDelayedChannel_0()
+    {
+        //
+        // less than one bin behind
+        //
+        MyConsumer con = new MyConsumer(null, 10L);
+
+        long[] aaa = {1, 3, 5,  10, 11, 18,  21, 23, 28,  30, 31, 37};
+        long[] bbb = {1, 3, 5,  10, 11, 18,  21, 23, 28,  30, 31, 37};
+        long[] ccc = {1, 3, 5,  -1, -1, 10,  11, 18,  21, 23 , 28,  30, 31, 37};
+        for(int idx=0; idx<ccc.length; idx++)
+        {
+            if(idx<aaa.length && aaa[idx] > 0)
+            {
+                con.pushData("aaa", aaa[idx]);
+            }
+            if(idx<bbb.length && bbb[idx] > 0)
+            {
+                con.pushData("bbb", bbb[idx]);
+            }
+            if(idx<ccc.length && ccc[idx] > 0)
+            {
+                con.pushData("ccc", ccc[idx]);
+            }
+        }
+
+        con.processAll();
+
+        int binStart = 1;
+        int binEnd = 9;
+        for(MyReport report: con.reports)
+        {
+            assertEquals(report.toString(), binStart, report.binStart);
+            assertEquals(report.toString(), binEnd, report.binEnd);
+            binStart= binEnd+1;
+            binEnd= binStart+9;
+        }
+
+        if (appender.getNumberOfMessages() > 0) {
+            fail("Saw log message(s)");
+        }
+
+    }
+
+
+    @Test
+    public void testDelayedChannel_1()
+    {
+        //
+        // one bin behind
+        //
+        MyConsumer con = new MyConsumer(null, 10L);
+
+        long[] aaa = {1, 3, 5,  10, 11, 18,  21, 23, 28,  30, 31, 37};
+        long[] bbb = {1, 3, 5,  10, 11, 18,  21, 23, 28,  30, 31, 37};
+        long[] ccc = {1, 3, 5,  -1, -1, -1,  10, 11, 18,  21, 23 , 28,  30, 31, 37};
+        for(int idx=0; idx<ccc.length; idx++)
+        {
+            if(idx<aaa.length && aaa[idx] > 0)
+            {
+                con.pushData("aaa", aaa[idx]);
+            }
+            if(idx<bbb.length && bbb[idx] > 0)
+            {
+                con.pushData("bbb", bbb[idx]);
+            }
+            if(idx<ccc.length && ccc[idx] > 0)
+            {
+                con.pushData("ccc", ccc[idx]);
+            }
+        }
+
+        con.processAll();
+
+        int binStart = 1;
+        int binEnd = 9;
+        for(MyReport report: con.reports)
+        {
+            assertEquals(report.toString(), binStart, report.binStart);
+            assertEquals(report.toString(), binEnd, report.binEnd);
+            binStart= binEnd+1;
+            binEnd= binStart+9;
+        }
+
+        if (appender.getNumberOfMessages() > 0) {
+            fail("Saw log message(s)");
+        }
+
+    }
+
+    @Test
+    public void testDelayedChannel_2()
+    {
+        //
+        // two bin behind
+        //
+        MyConsumer con = new MyConsumer(null, 10L);
+
+        long[] aaa = {1, 3, 5,  10, 11, 18,  21, 23, 28,  30, 31, 37};
+        long[] bbb = {1, 3, 5,  10, 11, 18,  21, 23, 28,  30, 31, 37};
+        long[] ccc = {1, 3, 5,  -1, -1, -1,  -1, -1, -1,  10, 11, 18,  21, 23 , 28,  30, 31, 37};
+        for(int idx=0; idx<ccc.length; idx++)
+        {
+            if(idx<aaa.length && aaa[idx] > 0)
+            {
+                con.pushData("aaa", aaa[idx]);
+            }
+            if(idx<bbb.length && bbb[idx] > 0)
+            {
+                con.pushData("bbb", bbb[idx]);
+            }
+            if(idx<ccc.length && ccc[idx] > 0)
+            {
+                con.pushData("ccc", ccc[idx]);
+            }
+        }
+
+        con.processAll();
+
+        int binStart = 1;
+        int binEnd = 9;
+        for(MyReport report: con.reports)
+        {
+            assertEquals(report.toString(), binStart, report.binStart);
+            assertEquals(report.toString(), binEnd, report.binEnd);
+            binStart= binEnd+1;
+            binEnd= binStart+9;
+        }
+
+        if (appender.getNumberOfMessages() > 0) {
+            fail("Saw log message(s)");
+        }
+
+    }
+
+    @Test
+    public void testDelayedChannel_3()
+    {
+        //
+        // two bin behind, no initial bin
+        //
+        MyConsumer con = new MyConsumer(null, 10L);
+
+        long[] aaa = {1, 3, 5,  10, 11, 18,  21, 23, 28,  30, 31, 37,  40, 45, 46};
+        long[] bbb = {1, 3, 5,  10, 11, 18,  21, 23, 28,  30, 31, 37,  40, 45, 46};
+        long[] ccc = {-1,-1,-1, -1, -1, -1,   1,  3,  5,  10, 11, 18,  21, 23, 28,  30, 31, 37,  40, 45, 46};
+        for(int idx=0; idx<ccc.length; idx++)
+        {
+            if(idx<aaa.length && aaa[idx] > 0)
+            {
+                con.pushData("aaa", aaa[idx]);
+            }
+            if(idx<bbb.length && bbb[idx] > 0)
+            {
+                con.pushData("bbb", bbb[idx]);
+            }
+            if(idx<ccc.length && ccc[idx] > 0)
+            {
+                con.pushData("ccc", ccc[idx]);
+            }
+        }
+
+        con.processAll();
+
+        int binStart = 1;
+        int binEnd = 9;
+        for(MyReport report: con.reports)
+        {
+            assertEquals(report.toString(), binStart, report.binStart);
+            assertEquals(report.toString(), binEnd, report.binEnd);
+            binStart= binEnd+1;
+            binEnd= binStart+9;
+        }
+
+        if (appender.getNumberOfMessages() > 0) {
+            fail("Saw log message(s)");
+        }
+
+    }
+
 }
